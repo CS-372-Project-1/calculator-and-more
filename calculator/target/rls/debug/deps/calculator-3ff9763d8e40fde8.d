d7115046cc1215ea7f33d1ee352c79b88bb37be6
eliacuna <elijahacuna@email.arizona.edu>
2021-09-21 12:02:32 -0700

    project init for calculator

---
+++ calculator/target/rls/debug/deps/calculator-3ff9763d8e40fde8.d
@@ -0,0 +1,5 @@
+/Users/eliacuna/Documents/calculator-and-more.nosync/calculator/target/rls/debug/deps/calculator-3ff9763d8e40fde8.rmeta: src/main.rs
+
+/Users/eliacuna/Documents/calculator-and-more.nosync/calculator/target/rls/debug/deps/calculator-3ff9763d8e40fde8.d: src/main.rs
+
+src/main.rs:
---
+++ calculator/target/rls/debug/deps/calculator-a1d95cdf3dd3e267.d
@@ -0,0 +1,5 @@
+/Users/eliacuna/Documents/calculator-and-more.nosync/calculator/target/rls/debug/deps/calculator-a1d95cdf3dd3e267.rmeta: src/main.rs
+
+/Users/eliacuna/Documents/calculator-and-more.nosync/calculator/target/rls/debug/deps/calculator-a1d95cdf3dd3e267.d: src/main.rs
+
+src/main.rs:
