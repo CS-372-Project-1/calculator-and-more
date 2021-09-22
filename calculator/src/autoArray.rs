82700c5b3594e06be1dc1d1786b183f8741f6535
eliacuna <elijahacuna@email.arizona.edu>
2021-09-22 11:52:56 -0700

    autoArray block comment added

--- calculator/src/autoArray.rs
+++ calculator/src/autoArray.rs
@@ -1,7 +1,10 @@
 /* the final feature of the Rust programming language that we wanted 
    to show off is a nifty thing called debug print statements.
    It is denoted by using {:?} in your string to print, and then
-   passing an array, vector, hashmap, or some other iterable structure
+   passing an array, vector, hashmap, or some other iterable structure.
+
+   Important to note: it is the only way to print an iterable other than
+   looping by index
 */
 
 pub fn run() {
